bdbe13c1d6141dfa1ac3a4d50b921b76dc284098
Дмитрий <dimsoz13@gmail.com>
2026-03-15 20:40:31 +0300

    final version

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_TITLE = "Siege of the Castle"
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title, center_window=True)
-        self.background_music = arcade.load_sound(":resources:/music/1918.mp3")
+        self.background_music = arcade.load_sound("music/1918.mp3")
         self.background_music.play(loop=True, volume=0.5)
 
 def main():
